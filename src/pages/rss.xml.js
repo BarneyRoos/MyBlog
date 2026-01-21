1bcf59c6831423db2b75a418dc460e70ea4b3721
BarneyRoos <2672973235@qq.com>
2026-01-21 21:01:28 +0800

    更新RSS订阅源信息

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journey learning Astro",
+    title: "海川的全栈之旅",
+    description: "JS全栈开发随笔",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-us</language>`,
