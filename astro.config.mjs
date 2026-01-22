7cba391841061257f4e68a055a13857d33982935
BarneyRoos <2672973235@qq.com>
2026-01-22 10:39:46 +0800

    移除preact，新增react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://full-stack-journey.netlify.app",
-  integrations: [preact()],
+  integrations: [react()],
 });
